4dcac0de515dd7720264ec942ecb2f81fe1416d2
Craig Dazey <craigdazey@gmail.com>
2018-06-06 00:05:27 -0400

    private parse functions, open file in binary mode

--- lib/dressing.ex
+++ lib/dressing.ex
@@ -13,17 +13,13 @@ defmodule Dressing do
 
   @type file_info :: {extension, mime_type}
 
-  # REVIEW: Looks like this number will end up being pretty big. It might be possible that we hit
-  # the end of some files before we get the desired buffer size. Maybe get file size and just read
-  # the whole thing if it's > this number?
-  #
   # We don't want to read the entire file, so we should just take as many bytes as we need.
-  @to_read 30
+  @to_read 150
 
   # TODO: Banged versions
   @spec get_mime_from_file(Path.t()) :: {:ok, file_info} | {:error, File.posix()} | IO.nodata()
   def get_mime_from_file(path) do
-    with {:ok, file} <- File.open(path, [:read]),
+    with {:ok, file} <- File.open(path, [:binary, :read]),
          binary when is_binary(binary) <- IO.binread(file, @to_read) do
       {:ok, parse(binary)}
     end
@@ -35,11 +31,11 @@ defmodule Dressing do
   end
 
   @spec parse(binary) :: file_info
-  def parse(<<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, _::binary>>),
+  defp parse(<<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, _::binary>>),
     do: {"png", "image/png"}
 
   # REVIEW: Handle text files?
-  def parse(<<0x47, 0x49, 0x46, _::binary>>), do: {"gif", "image/gif"}
-  def parse(<<0xFF, 0xD8, 0xFF, _::binary>>), do: {"jpg", "image/jpeg"}
-  def parse(_), do: {nil, "application/octet-stream"}
+  defp parse(<<0x47, 0x49, 0x46, _::binary>>), do: {"gif", "image/gif"}
+  defp parse(<<0xFF, 0xD8, 0xFF, _::binary>>), do: {"jpg", "image/jpeg"}
+  defp parse(_), do: {nil, "application/octet-stream"}
 end
